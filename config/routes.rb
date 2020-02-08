d2d82a8d2285086ee2afbf1e735a49d1bfa38a07
Arthur Galiullin <galiullinko@gmail.com>
2020-02-08 17:15:02 +0300

    Add show action in Project and Task controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :projects, only: %i(index create update destroy) do
-        resources :tasks, only: %i(index create update)
+      resources :projects, except: %i(new edit) do
+        resources :tasks, except: %i(new edit destroy)
       end
     end
   end
